9815ffa07a0767b5e9ded98bb19d668bda6b4c73
Maksim <vl-mail25@yandex.ru>
2018-05-31 16:20:11 +0700

    menu-top header

---
+++ html/partials/header.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="favicon.ico">
+
+    <title>Corde</title>
+
+    <link rel="stylesheet" href="/sass/main.css">
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+
+</head>
+<body>
+<div class="container">
+    <div class="row header">
+        <div class="col-md-3 justify-content-left">
+            <div class="main-logo">
+                <img src="/images/main_logo.png" alt="Логотип">
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="h-address">
+                <span>г. Москва, ул. Московская</span>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="h-mail">
+                <span>yourmail@gmail.com</span>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="h-contacts">
+                <h4>+7-(123)-456-78-90</h4>
+                <span>Время работы с 9 до 18</span>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ html/partials/menu_top.php
@@ -0,0 +1,20 @@
+<div class="container-fluid">
+    <div class="row top-nav">
+        <div class="col-md-6 d-flex justify-content-center">
+            <div class="dropdown">
+                <a class="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Dropdown link
+                </a>
+
+                <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                    <a class="dropdown-item" href="#">Action</a>
+                    <a class="dropdown-item" href="#">Another action</a>
+                    <a class="dropdown-item" href="#">Something else here</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-2 ml-auto">
+            <a href="#">О компании</a>
+        </div>
+    </div>
+</div>
